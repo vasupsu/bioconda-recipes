572ecdaf7072883fddbbba056bc01451d7458c80
Vasudevan Rengasamy <vxr162@psu.edu>
2018-07-11 20:46:27 -0400

    Updated build file

--- recipes/sprite4/build.sh
+++ recipes/sprite4/build.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+echo "prefix:" $PREFIX
 mkdir -p $PREFIX/bin
+mkdir -p $PREFIX/bin/strelka2_install
+builddir=$PREFIX/strelka2_install
+srcdir=$SRC_DIR/strelka2
+mkdir -p $builddir
 export CC=mpicc
 export CXX=mpicxx
 export CXXFLAGS=-DUSE_MPI
 
-cd minimap2_parallelio_circular; make; cp minimap2 $PREFIX/bin; cd ..
+cd minimap2; make; cp minimap2 genFastqIdx $PREFIX/bin; cd ..
+$CC -o $PREFIX/sampa -DUSE_MPI -DUSE_OMP sampa.c -fopenmp
+$CC -o $PREFIX/parsnip parsnip.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate -DUSE_OMP -fopenmp
+$CC -o $PREFIX/bamHeaderFile bamHeaderFile.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate
+
+cd $builddir
+$srcdir/configure --jobs=1 --prefix=$builddir
+make -j1 install
+rm -r bootstrap
+cd ..
