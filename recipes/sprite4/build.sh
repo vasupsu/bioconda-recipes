480b2112c2ee27bb53acd8401b6488441e6a8bb9
VASUDEVAN RENGASAMY <vxr162@comp-bc-0230.acib.production.int.aci.ics.psu.edu>
2018-09-06 02:42:15 -0400

    Modified build.sh to create soft links for starling2

--- recipes/sprite4/build.sh
+++ recipes/sprite4/build.sh
@@ -2,8 +2,7 @@
 
 echo "prefix:" $PREFIX
 mkdir -p $PREFIX/bin
-mkdir -p $PREFIX/bin/strelka2_install
-builddir=$PREFIX/strelka2_install
+builddir=$PREFIX/bin/strelka2_install
 srcdir=$SRC_DIR/sprite4_strelka2_modified
 mkdir -p $builddir
 export CC=mpicc
@@ -11,12 +10,14 @@ export CXX=mpicxx
 export CXXFLAGS=-DUSE_MPI
 
 cd sprite4_minimap2_modified; make; cp minimap2 genFastqIdx $PREFIX/bin; cd ..
-$CC -o $PREFIX/sampa -DUSE_MPI -DUSE_OMP sampa.c -fopenmp
-$CC -o $PREFIX/parsnip parsnip.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate -DUSE_OMP -fopenmp
-$CC -o $PREFIX/bamHeaderFile bamHeaderFile.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate
+$CC -o $PREFIX/bin/sampa -DUSE_MPI -DUSE_OMP sampa.c -fopenmp
+$CC -o $PREFIX/bin/parsnip parsnip.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate -DUSE_OMP -fopenmp
+$CC -o $PREFIX/bin/bamHeaderFile bamHeaderFile.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate
 
 cd $builddir
-$srcdir/configure --jobs=1 --prefix=$builddir
-make -j1 install
+$srcdir/configure --jobs=4 --prefix=$builddir
+make -j4 install
 rm -r bootstrap
 cd ..
+
+ln -s $builddir/libexec/GetChromDepth $builddir/libexec/starling2 $PREFIX/bin
