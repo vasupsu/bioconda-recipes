01c932ddab47bdc150b59624a3da6addb64ba0ad
VASUDEVAN RENGASAMY <vxr162@aci-lgn-005.acib.production.int.aci.ics.psu.edu>
2018-10-23 17:55:31 -0400

    mpi and non mpi compatibility

--- recipes/sprite4/build.sh
+++ recipes/sprite4/build.sh
@@ -5,19 +5,39 @@ mkdir -p $PREFIX/bin
 builddir=$PREFIX/bin/sprite4_strelka2_install
 srcdir=$SRC_DIR/sprite4_strelka2_modified
 mkdir -p $builddir
-export CC=mpicc
-export CXX=mpicxx
-export CXXFLAGS=-DUSE_MPI
+
+MPICC=`which mpicc || true`
+if test -z $MPICC
+then
+	echo "No-MPI compilation"
+	export CC=gcc
+	export CXX=g++
+else
+	echo "MPI compilation"
+	export CC=mpicc
+	export CXX=mpicxx
+	export MPIFLAG=' -DUSE_MPI'
+	export CXXFLAGS=' -DUSE_MPI'
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+	echo "Compiling sprite4 for OSX"
+	export ECFLAG=' -Xpreprocessor'
+	export ELFLAG=' -lomp'
+	export CXXFLAGS=' -stdlib=libc++'
+fi
 
 cp sprite4 sprite4-test $PREFIX/bin
 cd sprite4_minimap2_modified; make; cp sprite4-minimap2 genFastqIdx $PREFIX/bin; cd ..
-$CC -o $PREFIX/bin/sampa -DUSE_MPI -DUSE_OMP sampa.c -fopenmp
-$CC -o $PREFIX/bin/parsnip parsnip.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate -DUSE_OMP -fopenmp
-$CC -o $PREFIX/bin/bamHeaderFile bamHeaderFile.c -DUSE_MPI -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate
+echo "Compiling sampa EC ${ECFLAG} EL ${ELFLAG}"
+$CC -o $PREFIX/bin/sampa ${MPIFLAG} -DUSE_OMP sampa.c ${ECFLAG} -fopenmp ${ELFLAG}
+echo "Compiling parsnip"
+$CC -o $PREFIX/bin/parsnip parsnip.c -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate ${MPIFLAG} -DUSE_OMP ${ECFLAG} -fopenmp ${ELFLAG}
+$CC -o $PREFIX/bin/bamHeaderFile bamHeaderFile.c -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate
 
 cd $builddir
 $srcdir/configure --jobs=4 --prefix=$builddir
-make -j4 install
+make -j1 VERBOSE=1 install
 rm -r bootstrap
 cd ..
 
