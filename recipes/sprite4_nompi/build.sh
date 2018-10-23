b1fc1a6fe588d6503b7feeb327ba5fe0b29e4d2f
Vasudevan Rengasamy <vxr162@supports-MacBook-Air.local>
2018-10-23 16:50:51 -0400

    support openmp for osx

--- recipes/sprite4_nompi/build.sh
+++ recipes/sprite4_nompi/build.sh
@@ -10,14 +10,17 @@ export CXX=g++
 
 if [ "$(uname)" == "Darwin" ]; then
 	echo "Compiling sprite4 for OSX"
+	export ECFLAG=' -Xpreprocessor'
+	export ELFLAG=' -lomp'
 	export CXXFLAGS=' -stdlib=libc++'
 fi
-export MPIFLAG=${CXXFLAGS}
 
-cp sprite4 $PREFIX/bin
+cp sprite4 sprite4-test $PREFIX/bin
 cd sprite4_minimap2_modified; make; cp sprite4-minimap2 genFastqIdx $PREFIX/bin; cd ..
-$CC -o $PREFIX/bin/sampa -DUSE_OMP sampa.c -fopenmp
-$CC -o $PREFIX/bin/parsnip parsnip.c -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate -DUSE_OMP -fopenmp
+echo "Compiling sampa EC ${ECFLAG} EL ${ELFLAG}"
+$CC -o $PREFIX/bin/sampa -DUSE_OMP sampa.c ${ECFLAG} -fopenmp ${ELFLAG}
+echo "Compiling parsnip"
+$CC -o $PREFIX/bin/parsnip parsnip.c -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate -DUSE_OMP ${ECFLAG} -fopenmp ${ELFLAG}
 $CC -o $PREFIX/bin/bamHeaderFile bamHeaderFile.c -I${PREFIX}/include/htslib -L${PREFIX}/lib -lhts -lz  -lbz2 -llzma -lpthread -ldeflate
 
 cd $builddir
